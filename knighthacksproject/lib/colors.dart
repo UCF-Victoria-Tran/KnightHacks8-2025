d31bdd94a6c5ef47d503e81dd63c3c03cd734ff8
UCF-Victoria-Tran <victoriaungtran@gmail.com>
2025-10-25 02:39:11 -0400

    adding colors file

---
+++ knighthacksproject/lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color primary = Color(0xFF92C1FA);
+const Color error = Color(0xFF801F2C);
+
+const Color bgLighter = Color(0xFF273349);
+const Color bgDarker = Color(0xFF0E141C);
+const Color bg = Color(0xFF1B2133);
+// const Color backgroundColorLight = Color();
+
+const Color majorTextColor = Color(0xFFDFE9F6);
+const Color minorTextColor = Color(0xFF90A2BA);
+const Color unfocusedTextColor = Color(0xFF3F4246);
